bc77e3471c1b801fd90bf0bec9d38c91c79fb6d4
Tin <72661783+Tinny-rajput@users.noreply.github.com>
2021-06-10 11:44:43 +0530

    Add files via upload

--- js/posts.js
+++ js/posts.js
@@ -9,7 +9,9 @@ var data = [
     "https://tinny-rajput.github.io/Digital-Clock/",
     "https://tinny-rajput.github.io/neonlove.github.io/",
     "https://tinny-rajput.github.io/CodePen-Home-Accessible-Image-Compare-github.io/",
-    "https://photocarousal.netlify.app/"
+    "https://photocarousal.netlify.app/",
+    "https://tinny-rajput.github.io/Bezozs-earing-calculator/",
+    "https://tinny-rajput.github.io/Credit-Card-Form---VueJs/"
 
 ]
 
